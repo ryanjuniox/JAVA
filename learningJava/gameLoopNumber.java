92bf5331dda6d66920fe80d9d198890be4adaa6f
ryanjuniox <ryanjunio848@gmail.com>
2024-07-05 19:55:03 -0300

    new class: Simple Loop Game

---
+++ learningJava/gameLoopNumber.java
@@ -0,0 +1,26 @@
+package learningJava;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class gameLoopNumber {
+    @SuppressWarnings({ "unused", "resource" })    public static void main(String[] args) {
+        int numeroAleatorio = new Random().nextInt(100);
+        Scanner leitura = new Scanner(System.in);
+
+        int numeroTentativas = 5;
+        int tentativaUsuario = 0;
+        while(numeroTentativas > 0){
+            System.out.print("Digite um número: ");
+            tentativaUsuario = leitura.nextInt();
+            numeroTentativas--;
+
+            if(tentativaUsuario == numeroAleatorio){
+                System.out.println("Você acertou!");
+                break;
+            }else{
+                System.out.println("Você errou! Tente novamente.");
+            }
+        }
+    }
+}
