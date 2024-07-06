991f0eab0874c2c7a0e3a2abf3a46bffe425eb02
ryanjuniox <ryanjunio848@gmail.com>
2024-07-06 14:14:25 -0300

    fixes: logic game

--- learningJava/gameLoopNumber.java
+++ learningJava/gameLoopNumber.java
@@ -4,13 +4,15 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class gameLoopNumber {
-    @SuppressWarnings({ "unused", "resource" })    public static void main(String[] args) {
+    @SuppressWarnings({ "unused", "resource" })   
+    public static void main(String[] args) {
         int numeroAleatorio = new Random().nextInt(100);
         Scanner leitura = new Scanner(System.in);
 
         int numeroTentativas = 5;
         int tentativaUsuario = 0;
         while(numeroTentativas > 0){
+            
             System.out.print("Digite um número: ");
             tentativaUsuario = leitura.nextInt();
             numeroTentativas--;
@@ -19,8 +21,14 @@ public class gameLoopNumber {
                 System.out.println("Você acertou!");
                 break;
             }else{
-                System.out.println("Você errou! Tente novamente.");
+                if(numeroAleatorio > numeroTentativas){
+                    System.out.println("Numero aleatório maior que o escolhido! Tente novamente.");
+                }else{
+                    System.out.println("Numero aleatório menor que o escolhido! Tente novamente.");
+                }
+               //System.out.println("Você errou! Tente novamente.");
             }
+
         }
     }
 }
